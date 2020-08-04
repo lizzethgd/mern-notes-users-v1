2feab2246a30c606a70cec1828080ef22cfc7594
lizzethgd <lizzethgd@gmail.com>
2020-08-04 22:06:51 +0300

    adding navbar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -6,28 +6,30 @@ import reactLogo from '../logo.svg'
 export default class Navigation extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-         <Navbar.Brand href="/">
-      <img
-        alt=""
-        src={reactLogo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      MERN
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <NavLink className="navlink" to="/">Home</NavLink>
-          <NavLink className="navlink" to="/about">About</NavLink>
-          <NavLink className="navlink" to="/contact">Contact</NavLink>
-          <NavLink className="navlink" to="/students">Students</NavLink>
-          <NavLink className="navlink" to="/students/add">Add</NavLink>
-          <Nav.Link href="http://localhost:3500/api/v.1.0/students">API</Nav.Link>
-        </Nav>
-        </Navbar.Collapse>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+             
+            <NavLink className="navbar-brand" to="/">
+                <img
+                alt=""
+                src={reactLogo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                />
+                {' '}
+                MERN NotesApp
+            </NavLink>
+           
+           
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <NavLink className="navlink" to="/">Notes</NavLink>
+                    <NavLink className="navlink" to="/create">Create Note</NavLink>
+                    <NavLink className="navlink" to="/user">Create User</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+            
       </Navbar>
         )
     }
